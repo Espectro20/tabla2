4e719a4899f1c52f589164af244133f10491ec48
Espectro20 <104633841+Espectro20@users.noreply.github.com>
2022-08-12 14:12:30 -0400

    Add files via upload

---
+++ tabla again/tabla again/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace tabla_again
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtAa = new System.Windows.Forms.TextBox();
+            this.txtAb = new System.Windows.Forms.TextBox();
+            this.txtAc = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtAe = new System.Windows.Forms.TextBox();
+            this.txtBe = new System.Windows.Forms.TextBox();
+            this.txtBd = new System.Windows.Forms.TextBox();
+            this.txtBc = new System.Windows.Forms.TextBox();
+            this.txtBb = new System.Windows.Forms.TextBox();
+            this.txtBa = new System.Windows.Forms.TextBox();
+            this.txtCe = new System.Windows.Forms.TextBox();
+            this.txtCd = new System.Windows.Forms.TextBox();
+            this.txtCc = new System.Windows.Forms.TextBox();
+            this.txtCb = new System.Windows.Forms.TextBox();
+            this.txtCa = new System.Windows.Forms.TextBox();
+            this.txtFila5 = new System.Windows.Forms.TextBox();
+            this.txtFila4 = new System.Windows.Forms.TextBox();
+            this.txtFila3 = new System.Windows.Forms.TextBox();
+            this.txtFila2 = new System.Windows.Forms.TextBox();
+            this.txtFila1 = new System.Windows.Forms.TextBox();
+            this.txtcol3 = new System.Windows.Forms.TextBox();
+            this.txtcol2 = new System.Windows.Forms.TextBox();
+            this.txtcol1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(469, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "hola";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtAa
+            // 
+            this.txtAa.Location = new System.Drawing.Point(85, 91);
+            this.txtAa.Name = "txtAa";
+            this.txtAa.Size = new System.Drawing.Size(100, 20);
+            this.txtAa.TabIndex = 1;
+            // 
+            // txtAb
+            // 
+            this.txtAb.Location = new System.Drawing.Point(85, 117);
+            this.txtAb.Name = "txtAb";
+            this.txtAb.Size = new System.Drawing.Size(100, 20);
+            this.txtAb.TabIndex = 2;
+            // 
+            // txtAc
+            // 
+            this.txtAc.Location = new System.Drawing.Point(85, 143);
+            this.txtAc.Name = "txtAc";
+            this.txtAc.Size = new System.Drawing.Size(100, 20);
+            this.txtAc.TabIndex = 3;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(85, 169);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(100, 20);
+            this.txtAd.TabIndex = 4;
+            // 
+            // txtAe
+            // 
+            this.txtAe.Location = new System.Drawing.Point(85, 195);
+            this.txtAe.Name = "txtAe";
+            this.txtAe.Size = new System.Drawing.Size(100, 20);
+            this.txtAe.TabIndex = 5;
+            // 
+            // txtBe
+            // 
+            this.txtBe.Location = new System.Drawing.Point(200, 195);
+            this.txtBe.Name = "txtBe";
+            this.txtBe.Size = new System.Drawing.Size(100, 20);
+            this.txtBe.TabIndex = 10;
+            // 
+            // txtBd
+            // 
+            this.txtBd.Location = new System.Drawing.Point(200, 169);
+            this.txtBd.Name = "txtBd";
+            this.txtBd.Size = new System.Drawing.Size(100, 20);
+            this.txtBd.TabIndex = 9;
+            // 
+            // txtBc
+            // 
+            this.txtBc.Location = new System.Drawing.Point(200, 143);
+            this.txtBc.Name = "txtBc";
+            this.txtBc.Size = new System.Drawing.Size(100, 20);
+            this.txtBc.TabIndex = 8;
+            // 
+            // txtBb
+            // 
+            this.txtBb.Location = new System.Drawing.Point(200, 117);
+            this.txtBb.Name = "txtBb";
+            this.txtBb.Size = new System.Drawing.Size(100, 20);
+            this.txtBb.TabIndex = 7;
+            // 
+            // txtBa
+            // 
+            this.txtBa.Location = new System.Drawing.Point(200, 91);
+            this.txtBa.Name = "txtBa";
+            this.txtBa.Size = new System.Drawing.Size(100, 20);
+            this.txtBa.TabIndex = 6;
+            // 
+            // txtCe
+            // 
+            this.txtCe.Location = new System.Drawing.Point(319, 195);
+            this.txtCe.Name = "txtCe";
+            this.txtCe.Size = new System.Drawing.Size(100, 20);
+            this.txtCe.TabIndex = 15;
+            // 
+            // txtCd
+            // 
+            this.txtCd.Location = new System.Drawing.Point(319, 169);
+            this.txtCd.Name = "txtCd";
+            this.txtCd.Size = new System.Drawing.Size(100, 20);
+            this.txtCd.TabIndex = 14;
+            // 
+            // txtCc
+            // 
+            this.txtCc.Location = new System.Drawing.Point(319, 143);
+            this.txtCc.Name = "txtCc";
+            this.txtCc.Size = new System.Drawing.Size(100, 20);
+            this.txtCc.TabIndex = 13;
+            // 
+            // txtCb
+            // 
+            this.txtCb.Location = new System.Drawing.Point(319, 117);
+            this.txtCb.Name = "txtCb";
+            this.txtCb.Size = new System.Drawing.Size(100, 20);
+            this.txtCb.TabIndex = 12;
+            // 
+            // txtCa
+            // 
+            this.txtCa.Location = new System.Drawing.Point(319, 91);
+            this.txtCa.Name = "txtCa";
+            this.txtCa.Size = new System.Drawing.Size(100, 20);
+            this.txtCa.TabIndex = 11;
+            // 
+            // txtFila5
+            // 
+            this.txtFila5.Location = new System.Drawing.Point(469, 195);
+            this.txtFila5.Name = "txtFila5";
+            this.txtFila5.Size = new System.Drawing.Size(100, 20);
+            this.txtFila5.TabIndex = 20;
+            // 
+            // txtFila4
+            // 
+            this.txtFila4.Location = new System.Drawing.Point(469, 169);
+            this.txtFila4.Name = "txtFila4";
+            this.txtFila4.Size = new System.Drawing.Size(100, 20);
+            this.txtFila4.TabIndex = 19;
+            // 
+            // txtFila3
+            // 
+            this.txtFila3.Location = new System.Drawing.Point(469, 143);
+            this.txtFila3.Name = "txtFila3";
+            this.txtFila3.Size = new System.Drawing.Size(100, 20);
+            this.txtFila3.TabIndex = 18;
+            // 
+            // txtFila2
+            // 
+            this.txtFila2.Location = new System.Drawing.Point(469, 117);
+            this.txtFila2.Name = "txtFila2";
+            this.txtFila2.Size = new System.Drawing.Size(100, 20);
+            this.txtFila2.TabIndex = 17;
+            // 
+            // txtFila1
+            // 
+            this.txtFila1.Location = new System.Drawing.Point(469, 91);
+            this.txtFila1.Name = "txtFila1";
+            this.txtFila1.Size = new System.Drawing.Size(100, 20);
+            this.txtFila1.TabIndex = 16;
+            // 
+            // txtcol3
+            // 
+            this.txtcol3.Location = new System.Drawing.Point(319, 250);
+            this.txtcol3.Name = "txtcol3";
+            this.txtcol3.Size = new System.Drawing.Size(100, 20);
+            this.txtcol3.TabIndex = 23;
+            // 
+            // txtcol2
+            // 
+            this.txtcol2.Location = new System.Drawing.Point(200, 250);
+            this.txtcol2.Name = "txtcol2";
+            this.txtcol2.Size = new System.Drawing.Size(100, 20);
+            this.txtcol2.TabIndex = 22;
+            // 
+            // txtcol1
+            // 
+            this.txtcol1.Location = new System.Drawing.Point(85, 250);
+            this.txtcol1.Name = "txtcol1";
+            this.txtcol1.Size = new System.Drawing.Size(100, 20);
+            this.txtcol1.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 366);
+            this.Controls.Add(this.txtcol3);
+            this.Controls.Add(this.txtcol2);
+            this.Controls.Add(this.txtcol1);
+            this.Controls.Add(this.txtFila5);
+            this.Controls.Add(this.txtFila4);
+            this.Controls.Add(this.txtFila3);
+            this.Controls.Add(this.txtFila2);
+            this.Controls.Add(this.txtFila1);
+            this.Controls.Add(this.txtCe);
+            this.Controls.Add(this.txtCd);
+            this.Controls.Add(this.txtCc);
+            this.Controls.Add(this.txtCb);
+            this.Controls.Add(this.txtCa);
+            this.Controls.Add(this.txtBe);
+            this.Controls.Add(this.txtBd);
+            this.Controls.Add(this.txtBc);
+            this.Controls.Add(this.txtBb);
+            this.Controls.Add(this.txtBa);
+            this.Controls.Add(this.txtAe);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.txtAc);
+            this.Controls.Add(this.txtAb);
+            this.Controls.Add(this.txtAa);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtAa;
+        private System.Windows.Forms.TextBox txtAb;
+        private System.Windows.Forms.TextBox txtAc;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtAe;
+        private System.Windows.Forms.TextBox txtBe;
+        private System.Windows.Forms.TextBox txtBd;
+        private System.Windows.Forms.TextBox txtBc;
+        private System.Windows.Forms.TextBox txtBb;
+        private System.Windows.Forms.TextBox txtBa;
+        private System.Windows.Forms.TextBox txtCe;
+        private System.Windows.Forms.TextBox txtCd;
+        private System.Windows.Forms.TextBox txtCc;
+        private System.Windows.Forms.TextBox txtCb;
+        private System.Windows.Forms.TextBox txtCa;
+        private System.Windows.Forms.TextBox txtFila5;
+        private System.Windows.Forms.TextBox txtFila4;
+        private System.Windows.Forms.TextBox txtFila3;
+        private System.Windows.Forms.TextBox txtFila2;
+        private System.Windows.Forms.TextBox txtFila1;
+        private System.Windows.Forms.TextBox txtcol3;
+        private System.Windows.Forms.TextBox txtcol2;
+        private System.Windows.Forms.TextBox txtcol1;
+    }
+}
+
